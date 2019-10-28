c1197bcec44f6c5a22deb52603079efab03c5e94
waiting-gy <904450727@qq.com>
2019-10-28 19:03:31 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -47,7 +47,7 @@ parser.add_argument('--confidence_threshold', default=0.01, type=float,
                     help='Detection confidence threshold')
 parser.add_argument('--top_k', default=5, type=int,
                     help='Further restrict the number of predictions to parse')
-parser.add_argument('--cuda', default=True, type=str2bool,
+parser.add_argument('--cuda', default=False, type=str2bool,
                     help='Use cuda to train model')
 parser.add_argument('--voc_root', default=VOC_ROOT,
                     help='Location of VOC root directory')
